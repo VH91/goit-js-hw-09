1d5d2660ca0d27dbbf06be59b83ea5bfc790f777
VH91 <hritsik.v.1991@gmail.com>
2022-05-27 00:29:38 +0300

    finished promises

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,32 @@
+import Notiflix from 'notiflix';
+const btnCreatePromise = document.querySelector('button[type="submit"]');
+const inputFirstDelay = document.querySelector('input[name="delay"]');
+const inputStep = document.querySelector('input[name="step"]');
+const inputAmount = document.querySelector('input[name="amount"]');
+
+btnCreatePromise.addEventListener('click', onSubmit);
+
+function onSubmit(e) {
+    e.preventDefault();
+    const firstDelay = inputFirstDelay.valueAsNumber;
+    const step = inputStep.valueAsNumber;
+    const amount = inputAmount.valueAsNumber;
+    let timerCount = 0;
+    for (let i = 1; i <= amount; i += 1) {
+        createPromise(i, timerCount + firstDelay);
+        timerCount += step;
+    }
+}
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+    const promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const shouldResolve = Math.random() > 0.3;
+            if (shouldResolve) {
+                Notiflix.Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
+            } else {
+                Notiflix.Notify.failure(`Rejected promise ${position} in ${delay}ms`);
+            }
+        }, delay);
+    });
 }
